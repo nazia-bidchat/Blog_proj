f0a46f8cbb01f4444076a785e30fa3dcd0daadcc
Nazia Mathihalli <nazia.mathihalli@bidchat.com>
2018-02-12 19:10:45 +0530

    new changes 3

--- models/users.js
+++ models/users.js
@@ -2,6 +2,9 @@
 var bcrypt = require('bcrypt');
 var sha1=require('sha1');
 var jwt = require('jsonwebtoken');
+var config = require('../config/config1');
+var VerifyToken = require('../controllers/VerifyToken');
+
 module.exports = (sequelize, DataTypes) => {
   var users = sequelize.define('user', {
     'id': {
@@ -35,11 +38,17 @@ module.exports = (sequelize, DataTypes) => {
 
 
 
-  users.getName = function(details ,callback){
+  users.getName = function(req ,callback){
     //callback(null,true);
-users.findAll({
+users.findOne({
         attributes:['fname'],
-      }).then(function(result){
+      },
+    {
+      where:
+      {
+        id:req.params['id'],
+      }
+    }).then(function(result){
         callback(null,result);
     })
     .catch(function(error){
@@ -91,6 +100,39 @@ users.enterUser=function(req,callback)
 
 };
 
+users.enterUser1=function(req,res)
+{
+  users.create(
+    {
+      fname:req.body.fname,//"aaaa",
+      lastname:req.body.lastname,//"defgh",
+      //password:sha1(req.body.password),
+
+      password:bcrypt.hashSync(req.body.password,8),
+      id:req.params['id'],
+      //console.log(password);
+    //  console.log(fname);
+  }).then(function(user)
+    {
+      var token = jwt.sign({ id: user._id }, config.secret, {
+    expiresIn: 86400 // expires in 24 hours
+  });
+
+  res.status(200).send({ auth: true, token: token });
+  })  .catch(function(error){
+      console.log("error",error);
+     return res.status(500).send("There was a problem registering the user`.");
+
+      });
+
+};
+
+
+
+
+
+
+
 
 
 users.update1=function(req,callback)
@@ -119,27 +161,74 @@ users.update1=function(req,callback)
 };
 
 
-users.login1=function(req,callback)
+users.login1=function(req,res)
 {
-  users.findOne(
-    { where:
-      {fname: req.body.fname  }}).then(function(req,user)
+// //   users.findOne(
+// //     { where:
+// //       {fname: req.body.fname  }}).then(function(req,user)
+// //   {
+// //     var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+// //        if(passwordIsValid)
+// //        {
+// //          var token = jwt.sign({ id: user._id }, config.secret, {
+// //   expiresIn: 86400 // expires in 24 hours
+// // });
+// // }callback(null,user);}).catch(function(error){
+// //   return callback({message:error.message});
+// // });
+// // };
+//
+//
+//
+users.findOne(
   {
-    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-       if(passwordIsValid)
-       {
-         var token = jwt.sign({ id: user._id }, config.secret, {
-  expiresIn: 86400 // expires in 24 hours
-});
-}callback(null,user);}).catch(function(error){
-  return callback({message:error.message});
-});
-};
+    where:
+  { fname: req.body.fname }}).then( function ( user) {
+  // if (err) return res.status(500).send(user);
+  // console.log("im in err");
+  // if (!user) return  res.status(404).send('No user found.');
+  // console.log("im in user");
+  // check if the password is valid
+    console.log(user.password);
+  var pass =  bcrypt.hashSync(req.body.password,8);
+  var passwordIsValid = bcrypt.compare(pass, user.password);
+  if (!passwordIsValid)
+  // console.log(req.body.password);
+  //   console.log(passwordIsValid);
+
+  return res.status(401).send({ auth: false, token: null });
+
+  console.log("im in pass");
 
 
+  // if user is found and password is valid
+  // create a token
+  var token = jwt.sign({ id: user._id }, config.secret, {
+    expiresIn: 86400 // expires in 24 hours
+  });
+return   res.status(200).send({ auth: true, token: token });
+ // return the information including token as JSON
+
+}).catch(function(error){
+    console.log("error",error);
+    return res.status(500).send("user");
+    // return callback({
+    //     message:error.message
+    //   });
+    });
 
 
+}
 
+
+// users.verify(VerifyToken,function(req,res,next)
+// {
+//   users.findById(req.userId, { password: 0 }, function (err, user) {
+//    if (err) return res.status(500).send("There was a problem finding the user.");
+//    if (!user) return res.status(404).send("No user found.");
+//    res.status(200).send(user);
+// });
+// });
   console.log("users : ",users);
 
   return users;
--- route/user.js
+++ route/user.js
@@ -3,14 +3,29 @@ var router 			= express.Router();
 var app         = express();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
-
+var VerifyToken = require('../controllers/VerifyToken');
+var models = require('../models');
 var UserController = require('../controllers/usercontroller');
 //console.log('hi' + JSON.stringify(UserController.getname));
 router.get('/user/:id', UserController.getName);
 router.get('/user',UserController.getAll);
 router.post('/user',UserController.enterUser);
+router.post('/user1',UserController.enterUser1);
 router.put('/user/:id',UserController.update1);
 router.post('/login',UserController.login1);
+// router.get('/:id',UserController.verify);
+
+router.get('/me', VerifyToken, function(req, res, next) {
+console.log(req.userId);
+  models.user.findById(req.userId, { password: 0 }, function (err, user) {
+    if (err) return res.status(500).send("There was a problem finding the user.");
+    if (!user) return res.status(404).send("No user found.");
+     res.status(200).send(user);
+  //  next(user);
+  });
+
+});
+
 router.post('/caaat',  function(req, res)
 {
   console.log(req);
