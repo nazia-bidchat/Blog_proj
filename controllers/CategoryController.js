0d9b0ccc5905c260af9adb49d7017512c1e305d3
Nazia Mathihalli <nazia.mathihalli@bidchat.com>
2018-02-19 18:06:50 +0530

    final changes made

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -4,13 +4,11 @@ var categories=function(){
 };
 categories.getAllcategory=function(req,res)
 {
-
   models.category.getAllcategory (function(err,response)
   {
     if(err)
     {
       return res.status(404).send(response);
-
     }
     return res.status(200).send(response);
   });
@@ -21,13 +19,13 @@ categories.enterCat=function(req,res)
 {
   models.category.enterCat(req,function(err,response)
   {
-  if(err)
-  {
-    return res.status(500).send(response);
+    if(err)
+    {
+      return res.status(500).send(response);
 
-  }
-  return res.status(200).send(response);
-});
+    }
+    return res.status(200).send(response);
+  });
 };
 
 module.exports=categories;
